a598c69a1c16ef3548e9a723f4304978ce88c182
John Kilburg <john@greenjeans.com>
2015-09-14 15:41:17 -0400

    And add siocgifconf to the module.

--- netdevice.ml
+++ netdevice.ml
@@ -8,7 +8,9 @@ external siocgifmtu     : int -> string -> (int,int) Result.t   = "siocgifmtu_c"
 external siocgifaddr    : int -> string -> (int32,int) Result.t = "siocgifaddr_c"
 external siocgifbrdaddr : int -> string -> (int32,int) Result.t = "siocgifbrdaddr_c"
 external siocgifnetmask : int -> string -> (int32,int) Result.t = "siocgifnetmask_c"
-external siocgifhwaddr  : int -> string -> (bytes,int) Result.t = "siocgifhwaddr_c"       
+external siocgifhwaddr  : int -> string -> (bytes,int) Result.t = "siocgifhwaddr_c"
+  
+external siocgifconf : int -> ((string * int32) list, int) Result.t = "siocgifconf_c"
   
 external siocsifflags  : int -> string -> int -> (unit,int) Result.t  = "siocsifflags_c"
 external siocsifpflags : int -> string -> int -> (unit,int) Result.t  = "siocsifpflags_c"
